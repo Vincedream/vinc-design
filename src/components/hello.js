0584d6be85ce8c829d93c070d3d2d28e80045614
jingchuan <3161141062@qq.com>
2019-03-28 23:26:53 +0800

    init: project

---
+++ src/components/hello.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './hello.css'
+
+class Hello extends React.Component {
+  render() {
+    return (
+      <div className="hello">
+          hello 
+      </div>
+    );
+  }
+}
+
+module.exports =  Hello
